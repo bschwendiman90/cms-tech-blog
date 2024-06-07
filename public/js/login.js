608ba2815981be3c66962d4344d127e4d4d27d5e
Ben Schwendiman <bschwendiman90@gmail.com>
2024-06-06 20:51:19 -0600

    sign up and log in

--- public/js/login.js
+++ public/js/login.js
@@ -9,10 +9,10 @@ const loginFormHandler = async (event) => {
       // Send a POST request to the API endpoint
       const response = await fetch('/api/users/login', {
         method: 'POST',
-        body: JSON.stringify({ email, password }),
+        body: JSON.stringify({ name, password }),
         headers: { 'Content-Type': 'application/json' },
       });
-)
+      
       if (response.ok) {
         // If successful, redirect the browser to the profile page
         document.location.replace('/dashboard');
@@ -23,32 +23,9 @@ const loginFormHandler = async (event) => {
     }
   };
   
-  const signupFormHandler = async (event) => {
-    event.preventDefault();
-  
-    const name = document.querySelector('#name-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
-  
-    if (name && password) {
-      const response = await fetch('/api/users', {
-        method: 'POST',
-        body: JSON.stringify({ name, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/profile');
-      } else {
-        alert(response.statusText);
-      }
-    }
-  };
-  
+
   document
     .querySelector('.login-form')
     .addEventListener('submit', loginFormHandler);
-  
-  document
-    .querySelector('.signup-form')
-    .addEventListener('submit', signupFormHandler);
+
   
